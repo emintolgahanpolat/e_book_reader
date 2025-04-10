6f572af67db57fb9583a4f05bc7b32db0e0584c9
emintolgahanpolat <emintolgahanpolat@gmail.com>
2025-04-10 09:29:02 +0300

    -

--- lib/reader_content.dart
+++ lib/reader_content.dart
@@ -32,67 +32,85 @@ class _ReaderContentState extends State<ReaderContent> {
             widget.loadingWidget ?? const CircularProgressIndicator.adaptive(),
       );
     }
-    var pagesTexts = TextPaginator(
-      text: controller.text!,
-      textStyle: controller.config.textStyle,
-      pageSize: Size(
-        MediaQuery.of(context).size.width -
-            (controller.config.padding.horizontal * 2),
-        MediaQuery.of(context).size.height -
-            (controller.config.padding.vertical * 2),
-      ),
-    ).paginate();
-    var pages = [
-      if (widget.previousPageBuilder != null)
-        Container(
-          color: controller.config.backgroundColor,
-          child: widget.previousPageBuilder!(context),
-        ),
-      if (widget.previousPageBuilder == null && widget.coverPageBuilder != null)
-        Container(
-          color: controller.config.backgroundColor,
-          child: widget.coverPageBuilder!(context),
-        ),
-      for (var i = 0; i < pagesTexts.length; i++)
-        Container(
-          color: controller.config.backgroundColor,
-          padding: controller.config.padding,
-          child: Text(
-            pagesTexts[i],
-            style: controller.config.textStyle,
-          ),
-        ),
-      if (widget.nextPageBuilder != null)
-        Container(
-            color: controller.config.backgroundColor,
-            child: widget.nextPageBuilder!(context)),
-    ];
-    return Stack(
-      children: [
-        if (controller.currentPage < pages.length)
-          pages[controller.currentPage],
-        PageView.builder(
-          controller: controller.pageController,
-          itemCount: pages.length,
-          scrollDirection: controller.config.axis,
-          pageSnapping:
-              widget.coverPageBuilder != null && controller.currentPage == 1
-                  ? true
-                  : widget.nextPageBuilder != null &&
-                          controller.currentPage - 1 == pagesTexts.length
-                      ? true
-                      : controller.config.axis == Axis.horizontal,
-          physics: const ScrollPhysics(),
-          itemBuilder: (context, index) {
-            final isCurrentPage = index == controller.currentPage;
 
-            return Opacity(
-              opacity: isCurrentPage ? 0.0 : 1.0,
-              child: pages[index],
-            );
-          },
+    return LayoutBuilder(builder: (context, box) {
+      var pagesTexts = TextPaginator(
+        text: controller.text!,
+        textStyle: controller.config.textStyle,
+        pageSize: Size(
+          box.maxWidth - (controller.config.padding.horizontal * 2),
+          box.maxHeight - (controller.config.padding.vertical * 2),
         ),
-      ],
-    );
+      ).paginate();
+      var pages = [
+        if (widget.previousPageBuilder != null)
+          Container(
+            color: controller.config.backgroundColor,
+            child: widget.previousPageBuilder!(context),
+          ),
+        if (widget.previousPageBuilder == null &&
+            widget.coverPageBuilder != null)
+          Container(
+            color: controller.config.backgroundColor,
+            child: widget.coverPageBuilder!(context),
+          ),
+        for (var i = 0; i < pagesTexts.length; i++)
+          Container(
+            decoration: BoxDecoration(
+              color: controller.config.backgroundColor,
+              boxShadow: [
+                if (controller.config.axis == Axis.horizontal)
+                  const BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.0, 1.0), //(x,y)
+                    blurRadius: 6.0,
+                  ),
+              ],
+            ),
+            padding: controller.config.padding,
+            child: Text(
+              pagesTexts[i],
+              textAlign: controller.config.textAlign,
+              style: controller.config.textStyle,
+            ),
+          ),
+        if (widget.nextPageBuilder != null)
+          Container(
+              color: controller.config.backgroundColor,
+              child: widget.nextPageBuilder!(context)),
+      ];
+      return Stack(
+        fit: StackFit.expand,
+        children: [
+          if (controller.currentPage < pages.length)
+            pages[controller.currentPage],
+          PageView.builder(
+            controller: controller.pageController,
+            itemCount: pages.length,
+            scrollDirection: controller.config.axis,
+            pageSnapping:
+                widget.coverPageBuilder != null && controller.currentPage == 1
+                    ? true
+                    : widget.nextPageBuilder != null &&
+                            controller.currentPage - 1 == pagesTexts.length
+                        ? true
+                        : controller.config.axis == Axis.horizontal,
+            physics: const ScrollPhysics(),
+            itemBuilder: (context, index) {
+              final isCurrentPage = index == controller.currentPage;
+
+              return Opacity(
+                opacity: controller.config.axis == Axis.vertical
+                    ? 1
+                    : isCurrentPage
+                        ? 0.0
+                        : 1.0,
+                child: pages[index],
+              );
+            },
+          ),
+        ],
+      );
+    });
   }
 }
